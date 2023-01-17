52ae48da518a708c63b9fd2222c248d8f2e87755
jocon15 <jaywoc12@gmail.com>
2023-01-17 12:24:07 -0500

    implemented JSON for hbs pages travel, rooms, and news

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const articles = JSON.parse(fs.readFileSync('./data/news.json', 'utf8'));
+
 /* GET news view */
 const news = (req, res) => {
-    res.render('news', {title: 'Travlr Getaways'});
+    res.render('news', {title: 'Travlr Getaways', articles});
 };
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const availableRooms = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 const rooms = (req, res) => {
-    res.render('rooms', {title: 'Travlr Getaways'});
+    res.render('rooms', {title: 'Travlr Getaways', availableRooms});
 };
 
 module.exports = {
