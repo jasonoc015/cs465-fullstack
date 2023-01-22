1c9733fb6c8ddce8c42e44a75eeef2c26a0072e2
jocon15 <jaywoc12@gmail.com>
2023-01-21 23:23:30 -0500

    added json with hbs abstraction

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const foods = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 const meals = (req, res) => {
-    res.render('meals', {title: 'Travlr Getaways'});
+    res.render('meals', {title: 'Travlr Getaways', foods});
 };
 
 module.exports = {
